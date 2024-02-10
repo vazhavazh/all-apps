25238fd1b29b1d59a9b726fffb370222f9aaeefe
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-02-10 15:43:24 +0200

    accordion

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -3,16 +3,36 @@
 
 import data from "./data";
 import { useState } from "react";
+import "./style.css";
 
 export default function Accordian() {
 	const [selected, setSelected] = useState(null);
+	const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+	const [multiple, setMultiple] = useState([]);
 
 	const handleSingleSelection = (currentId) => {
 		setSelected(currentId === selected ? null : currentId);
+		console.log(currentId);
 	};
-	console.log(selected);
+	const handleEnableButton = () => {
+		setEnableMultiSelection(!enableMultiSelection);
+	};
+	const handleMultiSelection = (currentId) => {
+		let copyOfMultiplySelection = [...multiple];
+
+		const indexOfCurrentId = copyOfMultiplySelection.indexOf(currentId);
+		console.log(indexOfCurrentId);
+		if (indexOfCurrentId === -1) {
+			copyOfMultiplySelection.push(currentId);
+		} else {
+			copyOfMultiplySelection.splice(indexOfCurrentId, 1);
+		}
+		setMultiple(copyOfMultiplySelection);
+	};
+	console.log(selected, multiple);
 	return (
 		<div className='wrapper'>
+			<button onClick={handleEnableButton}>Enable Multi selection</button>
 			<div className='accordian'>
 				{data && data.length > 0 ? (
 					data.map((dataEl) => (
@@ -20,14 +40,25 @@ export default function Accordian() {
 							key={dataEl.id}
 							className='item'>
 							<div
-								onClick={() => handleSingleSelection(dataEl.id)}
+								onClick={() =>
+									enableMultiSelection
+										? handleMultiSelection(dataEl.id)
+										: handleSingleSelection(dataEl.id)
+								}
 								className='title'>
 								<h3>{dataEl.question}</h3>
 								<span>+</span>
 							</div>
-							{selected === dataEl.id ? (
+							{enableMultiSelection
+								? multiple.indexOf(dataEl.id) !== -1 && (
+										<div className='content'>{dataEl.answer}</div>
+								  )
+								: selected === dataEl.id && (
+										<div className='content'>{dataEl.answer}</div>
+								  )}
+							{/* {selected === dataEl.id ? (
 								<div className='content'>{dataEl.answer}</div>
-							) : null}
+							) : null} */}
 						</div>
 					))
 				) : (
